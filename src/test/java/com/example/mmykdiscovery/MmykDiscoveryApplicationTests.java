34849a1922e6aeb88c2624cc365e18cd259f98e0
89360944@qq.com <89360944@qq.com>
2022-04-06 22:06:47 +0800

    Initial commit

---
+++ src/test/java/com/example/mmykdiscovery/MmykDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mmykdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmykDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
